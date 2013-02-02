287438bdbc82e1470ef26697a3711f51e2097a68
gg <gg@Debian-60-squeeze-64-minimal.(none)>
2013-02-02 08:48:13 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple mass emailer written. Two - separate - files must be provided:
+- The body of the email in HTML
+- The contacts file. One email address per line.
---
+++ mailer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+subject="subject"
+from="Renato Lopes <renato@renato.com>"
+html="body.html"
+emails="contacts.txt"
+
+send_email()
+{
+	if [ -z "$1" ]; then
+		echo "Error: an email address must be provided"
+	fi
+	export MAILTO=$1
+	export CONTENT=$html
+	(
+		echo "From: "$from
+		echo "To: "$1
+		echo "MIME-Version: 1.0"
+		echo "Subject: "$assunto
+		echo "Content-Type: text/html"
+		echo "Content-Disposition: inline"
+		cat $CONTENT
+	) | /usr/sbin/sendmail -t $MAILTO
+}
+
+for i in `cat $emails`
+do
+	send_email $i
+	echo "email sent to $i"
+done
